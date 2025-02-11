dea59ec3b7f14c378530f069610276d98687fbda
thenil3sh <nileshkumar.newdelhi@gmail.com>
2025-02-11 18:55:53 +0530

    bro is it up?

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::{fs, env};
+// use std::thread;
+// use std::time;
+
+fn main() {
+    let args : Vec<String> = env::args().collect();
+    let mode = &args[1];
+
+    println!("\n{args:?}\n");
+    let address = &args[2];
+    
+    println!("Mode selected : {mode}");
+    println!("File to read : {address}");
+
+
+    let oreo = fs::read_to_string(address).unwrap();
+    println!("{oreo}");
+}
