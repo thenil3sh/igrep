9d8b6994d25f154d0bf38f67fad77c7f1729e16f
thenil3sh <nileshkumar.newdelhi@gmail.com>
2025-02-14 21:59:26 +0530

    now uses &str's but with lifetimes :(

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,8 @@
-use std::{fs, env};
-// use std::thread;
-// use std::time;
+use std::env;
+use igrep::Config;
 
 fn main() {
-    let args : Vec<String> = env::args().collect();
-    let mode = &args[1];
+    let args = env::args().collect();
 
-    println!("\n{args:?}\n");
-    let address = &args[2];
-    
-    println!("Mode selected : {mode}");
-    println!("File to read : {address}");
-
-
-    let oreo = fs::read_to_string(address).unwrap();
-    println!("{oreo}");
+    println!("{:?}", Config::from(&args));
 }
